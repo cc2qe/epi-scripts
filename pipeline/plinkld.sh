3a0b2d619c9629f2702acbced97fe76ef4fd915b
Colby Chiang <colby@muhee.partners.org>
2024-02-05 09:55:23 -0500

    exclude multiallelics

--- pipeline/plinkld.sh
+++ pipeline/plinkld.sh
@@ -9,6 +9,20 @@ OUT_PREFIX=chroms/${CHROM}
 # A copy of the VCF without the genotypes, so that we can polarize `--a2-allele`
 zcat ${VCF_PREFIX}.vcf.gz | cut -f -8 > ${VCF_PREFIX}.noGT.vcf &&
 
+# The VCF file contains multi-allelic sites (on separate lines).
+# These should be removed because the multi-allelic LD
+# structure that will confound results. I should have addressed this
+# earlier in the pipeline but I overlooked it. (Note: plink's `--max-alleles 2`
+# does not work because these variants are on multiple lines.
+#
+# As a workaround, I will follow instructions from
+# https://groups.google.com/g/plink2-users/c/O5UdjS2AAW4 to exclude
+# the multiallelics based on position by variant id.
+cat ${VCF_PREFIX}.noGT.vcf \
+    | vawk '{ print $2,$3 }' \
+    | zapdups -k1 -v | cut -f 2 \
+    > ${VCF_PREFIX}.multiallelic.txt &&
+
 # make impact file, tab delimited file of functional impact by variant
 cat ${VCF_PREFIX}.noGT.vcf \
     | vawk --header 'I$AF>0' \
@@ -26,6 +40,7 @@ plink1.9 \
     --threads 4 \
     --memory 16000 \
     --vcf ${VCF_PREFIX}.vcf.gz \
+    --exclude ${VCF_PREFIX}.multiallelic.txt \
     --biallelic-only \
     --a2-allele ${VCF_PREFIX}.noGT.vcf 4 3 '#' \
     --make-bed \
